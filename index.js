e5f3eb0175947cb42690697c5d779d1440ec0c3f
LeonardoKrum <leonardo.krum@hotmail.com>
2024-06-24 19:23:21 -0300

    alteração

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-let resposta
+let resposta = 0;
 
-console.log("Toca algum instrumento?")
+console.log("Toca algum instrumento?");
 
-process.stdin.on("data", function(data) {
-    let entrada_usuario = data.toString().trim().toLowerCase()
+process.stdin.on("data", function (data) {
+    let entrada_usuario = data.toString().trim().toLowerCase();
 
-    if(!resposta) {
-        resposta = entrada_usuario
+    if (!resposta) {
+        resposta = entrada_usuario;
 
-        if(resposta == "s" || resposta == "sim") {
-            console.log("Qual?")
+        if (resposta == "s" || resposta == "sim") {
+            console.log("Qual?");
         } else if (resposta == "n" || resposta == "não" || resposta == "nao") {
-            console.log("Nada de bom")
-            process.exit()
+            console.log("Nada de bom");
+            process.exit();
         } else {
-            resposta = undefined
-            console.log("Não entendi")
+            resposta = undefined;
+            console.log("Não entendi");
         }
     } else {
-        if(entrada_usuario == "violão") {
-            console.log("digirigidum")
-        } else if(entrada_usuario == "guitarrra") {
-            console.log("Então vc é trovoso mesmo hein")
-        } else if(entrada_usuario == "bateria") {
-            console.log("Bater tambor")
+        if (entrada_usuario == "violão") {
+            console.log("digirigidum");
+        } else if (entrada_usuario == "guitarrra") {
+            console.log("Então vc é trovoso mesmo hein");
+        } else if (entrada_usuario == "bateria") {
+            console.log("Bater tambor");
         } else {
-            console.log("Instrumento esquisito")
+            console.log("Instrumento esquisito");
         }
     }
 });
